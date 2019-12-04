04f587b1d91ab994fe526e86e28b31aeb168b2ba
Jepassaispar <51320916+Jepassaispar@users.noreply.github.com>
2019-12-04 15:57:34 +0100

    images added

--- src/components/cocktails/CocktailCard.jsx
+++ src/components/cocktails/CocktailCard.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function CocktailCard({ cocktail }) {
-  return <div>{cocktail.Name}</div>;
+  return (
+    <>
+      <div>{cocktail.Name}</div>
+      <div>
+        <img src={cocktail.Image}></img>
+      </div>
+    </>
+  );
 }
