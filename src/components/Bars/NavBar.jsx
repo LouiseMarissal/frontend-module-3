1337f29d01cdfac358db803cd21ebcacc2c0bdc4
Louise Marissal <louisemarissal@MacBook-Pro-de-Louise.local>
2019-12-04 15:04:38 +0100

    little css for navbar

--- src/components/Bars/NavBar.jsx
+++ src/components/Bars/NavBar.jsx
@@ -4,18 +4,23 @@ import { NavLink } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav className="nav-bar">
-      <NavLink className="link" to="/">
-        Home
-      </NavLink>
-      <NavLink className="link" to="/login">
-        Login
-      </NavLink>
-      <NavLink className="link" to="/signup">
-        SignUp
-      </NavLink>
-      <NavLink className="link" to="/profile">
-        Profile
-      </NavLink>
+      <div className="home-nav">
+        <NavLink className="link" to="/">
+          Home
+        </NavLink>
+      </div>
+      <div>
+        <NavLink className="link" to="/login">
+          Login
+        </NavLink>
+        <NavLink className="link" to="/signup">
+          SignUp
+        </NavLink>
+        {/* If loggedIn */}
+        <NavLink className="link" to="/profile">
+          Profile
+        </NavLink>
+      </div>
     </nav>
   );
 }
