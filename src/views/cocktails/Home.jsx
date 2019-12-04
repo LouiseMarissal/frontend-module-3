2773d78b4debacc7739360f7c8ed57eaa0a6824a
Louise Marissal <louisemarissal@MacBook-Pro-de-Louise.local>
2019-12-04 11:57:36 +0100

    commit

--- src/views/cocktails/Home.jsx
+++ src/views/cocktails/Home.jsx
@@ -4,16 +4,6 @@ export default function Home() {
   return (
     <div>
       <h1 className="title">HomePage of Mixology Loveeers</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed est odio,
-        bibendum in leo nec, condimentum imperdiet arcu. Aliquam nec lacus orci.
-        Duis rutrum nisl commodo diam ullamcorper placerat. Sed porta efficitur
-        ipsum, sed viverra elit ultrices a. In hac habitasse platea dictumst.
-        Pellentesque vel mauris eu quam congue vehicula. Aliquam gravida mollis
-        dolor, ac mollis lorem ultrices ut. Proin venenatis semper tincidunt.
-        Donec volutpat faucibus nunc in pharetra. Ut non ornare tortor, in
-        venenatis mauris.
-      </p>
     </div>
   );
 }
