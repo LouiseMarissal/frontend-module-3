92f042bfa92c39dd36f94118d57ec2f8552ddedd
Nicolas BISCHOFF <nicolasbischoff@MacBook-Pro-de-Nicolas.local>
2019-12-06 11:33:47 +0100

    one cocktail

--- src/views/cocktails/OneCocktail.jsx
+++ src/views/cocktails/OneCocktail.jsx
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
 import "./../../css/OneCocktail.css";
 import axios from "axios";
+import { object } from "prop-types";
 
 export default function OneCocktail(props) {
-  console.log(props.match.params.id);
   const [cocktail, setCocktail] = useState([]);
+  // if (cocktail.Ingredients !== undefined) {
+  //   // console.log(cocktail.Ingredients);
+  //   // console.log(typeof cocktail.Ingredients);
+  //   // let test = Array.from(cocktail.Ingredients);
+  //   // console.log(test[0]);
+  // }
+
   useEffect(() => {
     axios
       .get(
@@ -13,42 +20,39 @@ export default function OneCocktail(props) {
       .then(dbRes => setCocktail(dbRes.data))
       .catch(err => console.log(err));
   }, []);
-  console.log(cocktail.Ingredients);
-  return (
-    <>
-      <div className="CocktailCard">
-        <div className="titre">{cocktail.Name}</div>
-        <div className="onecocktail">
-          <div>
-            <img className="OneCocktailImage" src={cocktail.Image} alt="" />
-          </div>
-          <div className="test"></div>
-          <div className="FullInstructions">
-            <div className="what">What do i need ?</div> <br />
-            <ul className="Ingredients">
-              <li>
-                <span className="ingredient">{cocktail.Ingredients} :</span>
-                <span className="mesure"> {cocktail.Measures}</span>
-              </li>
-              <li>
-                <span className="ingredient">{cocktail.Ingredient2} :</span>
-                <span className="mesure"> {cocktail.Measure2}</span>
-              </li>
-              <li>
-                <span className="ingredient">{cocktail.Ingredient3} :</span>
-                <span className="mesure"> {cocktail.Measure3}</span>
-              </li>
-              <li>
-                <span className="ingredient">{cocktail.Ingredient4} :</span>
-                <span className="mesure"> {cocktail.Measure4}</span>
-              </li>
-            </ul>
-            <br />
-            <div className="how">How do i make it ?</div> <br />
-            <div className="instructions">{cocktail.Instructions}</div>
+  if (cocktail.Ingredients !== undefined) {
+    return (
+      <>
+        <div className="CocktailCard">
+          <div className="titre">{cocktail.Name}</div>
+          <div className="onecocktail">
+            <div>
+              <img className="OneCocktailImage" src={cocktail.Image} alt="" />
+            </div>
+            <div className="test"></div>
+            <div className="FullInstructions">
+              <div className="what">What do i need ?</div> <br />
+              <ul className="Ingredients">
+                {cocktail.Ingredients.map((Ingredient, i) => {
+                  if (Ingredient !== "") {
+                    return <li className="ingredient">{Ingredient}</li>;
+                  } else return null;
+                })}
+              </ul>
+              <br />
+              <ul className="Measures">
+                {cocktail.Measures.map((Measure, i) => {
+                  if (Measure !== "") {
+                    return <li className="measure">({Measure})</li>;
+                  } else return null;
+                })}
+              </ul>
+              <div className="how">How do i make it ?</div> <br />
+              <div className="instructions">{cocktail.Instructions}</div>
+            </div>
           </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } else return null;
 }
