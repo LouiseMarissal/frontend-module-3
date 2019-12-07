efb31d4b74d6056f9d11fc08187328abd096850d
Jepassaispar <51320916+Jepassaispar@users.noreply.github.com>
2019-12-07 14:25:30 +0100

    blur effect

--- src/components/cocktails/CocktailsList.jsx
+++ src/components/cocktails/CocktailsList.jsx
@@ -1,13 +1,10 @@
 import React, { Suspense, lazy } from "react";
 import {} from "react";
-import "./../../css/CocktailList.css";
 
 const CocktailCard = React.lazy(() => import("./CocktailCard"));
 const List = ({ cocktails }) => {
   return (
-    <div
-      className="fullcocktail"
-    >
+    <div className="fullcocktail">
       {cocktails.map((cocktail, i) => (
         <Suspense key={i} fallback={<div>Loading...</div>}>
           <CocktailCard key={i} cocktail={cocktail} />
