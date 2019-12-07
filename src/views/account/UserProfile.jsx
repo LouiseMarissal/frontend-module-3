7b08cbc6cc04dfcd5d3ecb2331546e2075c8b194
Jepassaispar <51320916+Jepassaispar@users.noreply.github.com>
2019-12-07 18:11:14 +0100

    user form create almost done

--- src/views/account/UserProfile.jsx
+++ src/views/account/UserProfile.jsx
@@ -5,18 +5,14 @@ import CocktailList from "./../../components/cocktails/CocktailsList";
 import axios from "axios";
 
 export default function UserProfile(id) {
-  const [user, setUser] = useState({});
+  // const [user, setUser] = useState({});
 
-  const getUserCocktail = () => {
-    axios
-      .get(process.env.REACR_APP_BACKEND_URL, "/userCocktail/" + _userProID)
-      .then()
-      .catch();
-  };
+  // const getUserCocktail = () => {
+  //   axios
+  //     .get(process.env.REACR_APP_BACKEND_URL, "/userCocktail/" + _userProID)
+  //     .then()
+  //     .catch();
+  // };
 
-  return (
-    <div>
-      <CocktailList cocktails={} />
-    </div>
-  );
+  return <div>{/* <CocktailList cocktails={} /> */}</div>;
 }
