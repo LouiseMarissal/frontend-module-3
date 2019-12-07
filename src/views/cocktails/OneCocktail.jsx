9c16e42fd04c1dda2f0e828296fbb03fc28fe6c7
Justine Delebarre <justinedelebarre@MacBook-Pro-de-Justine-Delebarre.local>
2019-12-07 14:55:32 +0100

    add sass

--- src/views/cocktails/OneCocktail.jsx
+++ src/views/cocktails/OneCocktail.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
-import "./../../css/OneCocktail.css";
+import "./../../css/OneCocktail.scss";
 import axios from "axios";
-import { object } from "prop-types";
 
 export default function OneCocktail(props) {
   const [cocktail, setCocktail] = useState([]);
@@ -24,20 +23,20 @@ export default function OneCocktail(props) {
   if (cocktail.Ingredients !== undefined) {
     return (
       <>
-        <div className="CocktailCard">
+        <div className="cocktailView">  
+          <div className="container">       
           <div className="titre">{cocktail.Name}</div>
           <div className="onecocktail">
             <div>
               <img className="OneCocktailImage" src={cocktail.Image} alt="" />
             </div>
-            <div className="test"></div>
             <div className="FullInstructions">
               <div className="what">What do i need ?</div> <br />
               <div className="full-ingredients">
                 <ul className="Ingredients">
                   {cocktail.Ingredients.map((Ingredient, i) => {
                     if (Ingredient !== "" && Ingredient !== null) {
-                      return <li className="ingredient">{Ingredient}</li>;
+                      return <li key={i} className="ingredient">{Ingredient}</li>;
                     } else return null;
                   })}
                 </ul>
@@ -45,7 +44,7 @@ export default function OneCocktail(props) {
                 <ul className="Measures">
                   {cocktail.Measures.map((Measure, i) => {
                     if (Measure !== "" && Measure !== null) {
-                      return <li className="measure">({Measure})</li>;
+                      return <li key={i} className="measure">({Measure})</li>;
                     } else return null;
                   })}
                 </ul>
@@ -54,6 +53,7 @@ export default function OneCocktail(props) {
               <div className="instructions">{cocktail.Instructions}</div>
             </div>
           </div>
+          </div> 
         </div>
       </>
     );
