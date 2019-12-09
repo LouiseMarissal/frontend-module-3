8ee2c2dfc4f4799f7b1336f5ccd060cd4b062d14
Nicolas BISCHOFF <nicolasbischoff@MacBook-Pro-de-Nicolas.local>
2019-12-09 15:11:14 +0100

    comment

--- src/components/comments/FormComment.jsx
+++ src/components/comments/FormComment.jsx
@@ -1,54 +1,57 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default class FormComment extends Component {
-  state = {
-    message: ""
+const AddComment = props => {
+  const [message, setMessage] = useState({});
+  const [finalMessage, setFinalMessage] = useState();
+
+  const handleChange = e => {
+    setMessage({ ...message, [e.target.name]: e.target.value });
   };
 
-  handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    console.log(this.setState);
+    // console.log(props);
+    setFinalMessage(message.message);
     axios
-      .patch(process.env.REACT_APP_BACKEND_URL + "/user/")
+      .post(process.env.REACT_APP_BACKEND_URL + "/comment", {
+        message: message.message,
+        created: Date.now(),
+        cocktail: `${props.CocktailId}`
+      })
       .then(res => {
-        console.log(res);
-        // props.history.push("/cocktails");
+        console.log(res.data);
       })
       .catch(err => {
         console.log(err);
       });
   };
 
-  render() {
-    const { message } = this.state;
-    console.log({ message });
+  return (
+    <form
+      className="formComment"
+      onSubmit={handleSubmit}
+      onChange={handleChange}
+    >
+      <div className="row">
+        <input
+          className="input"
+          name="message"
+          id="message"
+          cols="30"
+          rows="10"
+          onChange={() => void 0}
+          placeholder="leave a comment here..."
+        ></input>
+        <button className="btn">send!</button>
+        {finalMessage == undefined ? (
+          <p>No message yet</p>
+        ) : (
+          <p>{finalMessage}</p>
+        )}
+      </div>
+    </form>
+  );
+};
 
-    return (
-      <form
-        className="formComment"
-        onSubmit={this.handleSubmit}
-        onChange={this.handleChange}
-      >
-        <div className="row">
-          <input
-            className="input"
-            name="message"
-            id="message"
-            cols="30"
-            rows="10"
-            onChange={() => void 0}
-            value={message}
-            placeholder="leave a comment here..."
-          ></input>
-
-          <button className="btn">send!</button>
-        </div>
-      </form>
-    );
-  }
-}
+export default AddComment;
